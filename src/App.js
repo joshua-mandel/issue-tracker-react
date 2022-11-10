0aa98ef0baefcfe1b8b7296c049e635fb4f26f48
joshua-mandel <89474480+joshua-mandel@users.noreply.github.com>
2022-11-10 15:29:32 -0600

    update

--- src/App.js
+++ src/App.js
@@ -2,36 +2,65 @@ import { Route, Routes, Link } from 'react-router-dom';
 import { BugEditor } from './components/BugEditor';
 import { BugList } from './components/BugList';
 import { BugListItem } from './components/BugListItem';
-import { LoginForm } from './components/LoginForm.js';
-import { RegisterForm } from './components/RegisterForm';
+import LoginForm from './components/LoginForm.js';
+import RegisterForm from './components/RegisterForm';
 import { UserEditor } from './components/UserEditor';
 import { UserList } from './components/UserList';
 import { UserListItem } from './components/UserListItem';
+import './styles/App.css'
 
 function App() {
   return (
     <>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-      </ul>
-    </nav>
-    <Routes>
-      <Route path="/" element={<LoginForm />} />
-      <Route path="/register" element={<RegisterForm />} />
-      <Route path="/bug-editor" element={<BugEditor />} />
-      <Route path="/bug-list" element={<BugList />} />
-      <Route path="/bug-list-item" element={<BugListItem />} />
-      <Route path="/login" element={<LoginForm />} />
-      <Route path="/user-editor" element={<UserEditor />} />
-      <Route path="/user-list" element={<UserList />} />
-      <Route path="/user-list-item" element={<UserListItem />} />
-    </Routes>
+      <div className='' id='background'>
+      <nav className="navbar navbar-expand-lg bg-light sticky-top">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Issue Tracker
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link></Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      
+      <Routes>
+        <Route path="/" element={ <LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/bug-editor" element={<BugEditor />} />
+        <Route path="/bug-list" element={<BugList />} />
+        <Route path="/bug-list-item" element={<BugListItem />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/user-editor" element={<UserEditor />} />
+        <Route path="/user-list" element={<UserList />} />
+        <Route path="/user-list-item" element={<UserListItem />} />
+      </Routes>
+      </div>
     </>
   );
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,3 +1,41 @@
-export function LoginForm() {
-  return <h1>Login Form</h1>
+import '../styles/LoginForm.css';
+
+function LoginForm() {
+  return (
+    <div className='container col-12 col-md-8 col-lg-6 col-xl-5'>
+      <div className='d-flex justify-content-center mt-5'>
+        <img className='' src='issue-tracker-logo-sm.png' alt='issue tracker logo'/>
+      </div>
+    <div className="mt-3" id="login-component">
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="">
+            <div className="card shadow-2-strong" id="rounded-corner">
+              <div className="card-body p-5 text-center">
+                <h3 className="mb-5">Sign in</h3>
+                <div className="d-flex form-outline mb-4 align-items-end">
+                  <input type="email" id="email-input" className="form-control form-control-lg" placeholder="Email" />
+                </div>
+                <div className="d-flex form-outline mb-4 align-items-end">
+                  <input
+                    type="password"
+                    id="email-input"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                  />
+                  
+                </div>
+                <button class="btn btn-primary btn-lg btn-block" type="submit">
+                  Login
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
 }
+
+export default LoginForm;
