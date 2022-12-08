c16259cb3c6f6b8c3071d5b4b4023bbc4a168320
joshua-mandel <89474480+joshua-mandel@users.noreply.github.com>
2022-12-08 00:39:23 -0600

    update

--- src/components/BugEditor.js
+++ src/components/BugEditor.js
@@ -1,10 +1,17 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import InputField from './InputField';
 import axios from 'axios';
 
 function BugEditor({ auth, showError }) {
   const { bugId } = useParams();
   const [bug, setBug] = useState(null);
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const [stepsToReproduce, setStepsToReproduce] = useState();
+  const [classification, setClassification] = useState();
+  const [assignedTo, setAssignedTo] = useState();
+  const [closed, setClosed] = useState();
   const [error, setError] = useState('');
   const [pending, setPending] = useState(true);
 
@@ -33,10 +40,16 @@ function BugEditor({ auth, showError }) {
       });
   }, [auth, showError, bugId]);
 
+  function onInputChange(evt, setValue) {
+    const newValue = evt.currentTarget.value;
+    setValue(newValue);
+    console.log(newValue);
+  }
+
   return (
     <div className="container">
-      <h3 className="mb-1">Bug Editor</h3>
-      <h5 className="mb-5">Welcome {auth.payload.fullName}</h5>
+      <h3 className="mb-2">Bug Editor</h3>
+      <h5 className="mb-4">Welcome {auth.payload.fullName}</h5>
       {pending && (
         <div className="spinner-border text-primary" role="status">
           <span className="visually-hidden">Loading...</span>
@@ -46,6 +59,14 @@ function BugEditor({ auth, showError }) {
       {bug && (
         <form>
           <div>BugId: {bugId}</div>
+          <InputField
+          label="Title:"
+          id="title-update"
+          autoComplete=""
+          value={title}
+          onChange={(evt) => onInputChange(evt, setTitle)}
+          error={error}
+        />
           <div>Title: {bug.title}</div>
           <div>Description: {bug.description}</div>
           <div>BugClass: {bug.bugClass}</div>
