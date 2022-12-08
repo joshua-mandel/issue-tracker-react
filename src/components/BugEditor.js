02d00c7b8e700f96ba784728d3db43ae6c9431ee
joshua-mandel <89474480+joshua-mandel@users.noreply.github.com>
2022-12-08 00:13:25 -0600

    update

--- src/components/BugEditor.js
+++ src/components/BugEditor.js
@@ -1,84 +1,58 @@
 import { useParams } from 'react-router-dom';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function BugEditor(props) {
-  const { _id } = useParams();
-
-  const navigate = useNavigate();
-
-  const [newTitle, setNewTitle] = useState();
-  const [newDescription, setNewDescription] = useState();
-  const [newBugClass, setNewBugClass] = useState();
+function BugEditor({ auth, showError }) {
+  const { bugId } = useParams();
+  const [bug, setBug] = useState(null);
+  const [error, setError] = useState('');
+  const [pending, setPending] = useState(true);
 
   function handleClick(e) {
     e.preventDefault();
-    navigate('/bug/list');
   }
 
-  return (
-    <div className="container col-12 col-md-10 col-lg-10 col-xl-10">
-      <div className="mt-5" id="login-component">
-        <div className="card shadow-2-strong" id="rounded-corner">
-          <div className="card-body p-5 text-center">
-            <h3 className="mb-1">Bug Editor</h3>
-            <h5 className="mb-5">Welcome {props.fullName}</h5>
-            <div>Bug Id: {_id}</div>
-            <form>
-              <div className="container">
-                <div className="d-flex form-outline row mb-4 align-items-center text-start">
-                  <div className="col-md-3">
-                    <label className="me-2">Title: </label>
-                  </div>
-                  <div className="col-md-9">
-                    <input
-                      type="text"
-                      id="title-input"
-                      className="form-control form-control-lg col-md-4"
-                      onChange={(evt) => setNewTitle(evt.currentTarget.value)}
-                      value={newTitle}
-                    />
-                  </div>
-                </div>
+  useEffect(() => {
+    setPending(true);
+    axios(`${process.env.REACT_APP_API_URL}/api/bug/${bugId}`, {
+      method: 'get',
+      headers: {
+        authorization: `Bearer ${auth?.token}`,
+      },
+    })
+      .then((res) => {
+        console.log(res.data);
+        setPending(false);
+        setBug(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setPending(false);
+        setError(err.message);
+        showError(err.message);
+      });
+  }, [auth, showError, bugId]);
 
-                <div className="d-flex form-outline row mb-4 align-items-center text-start">
-                  <div className="col-md-3">
-                    <label className="me-2">Description: </label>
-                  </div>
-                  <div className="col-md-9">
-                    <input
-                      type="text"
-                      id="description-input"
-                      className="form-control form-control-lg"
-                      onChange={(evt) => setNewDescription(evt.currentTarget.value)}
-                      value={newDescription}
-                    />
-                  </div>
-                </div>
-                <div className="d-flex form-outline mb-4 row align-items-center text-start">
-                  <div className='col-md-3'>
-                    <label className="me-2 ">BugClass: </label>
-                  </div>
-                  <div className='col-md-9'>
-                    <input
-                      type="text"
-                      id="bugClass-input"
-                      className="form-control form-control-lg"
-                      onChange={(evt) => setNewBugClass(evt.currentTarget.value)}
-                      value={newBugClass}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div>
-                <button className='btn btn-primary btn-lg btn-block' onClick={handleClick} type='submit'>
-                  Submit
-                </button>
-              </div>
-            </form>
-          </div>
+  return (
+    <div className="container">
+      <h3 className="mb-1">Bug Editor</h3>
+      <h5 className="mb-5">Welcome {auth.payload.fullName}</h5>
+      {pending && (
+        <div className="spinner-border text-primary" role="status">
+          <span className="visually-hidden">Loading...</span>
         </div>
-      </div>
+      )}
+      {error && <div className="text-danger mb-2">{error}</div>}
+      {bug && (
+        <form>
+          <div>BugId: {bugId}</div>
+          <div>Title: {bug.title}</div>
+          <div>Description: {bug.description}</div>
+          <div>BugClass: {bug.bugClass}</div>
+          <div>Assigned To: {bug.assignedTo}</div>
+          <div>Closed: {bug.closed.toString()}</div>
+        </form>
+      )}
     </div>
   );
 }
