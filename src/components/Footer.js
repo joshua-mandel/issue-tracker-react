12834f0fdb3be92152e7e0dc79c5fa50f1a28439
joshua-mandel <89474480+joshua-mandel@users.noreply.github.com>
2022-12-10 01:42:07 -0600

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,17 @@ function Footer() {
   return (
     <footer className="bg-dark text-white p-2">
       <div className="container">
-        <div className='d-flex align-items-center justify-content-between'>
+        <div className="d-flex align-items-center justify-content-between">
           <div>&copy; Joshua Mandel 2022</div>
-          <div className='d-flex align-items-center'>
-            <div className='me-2'>Built with</div>
-            <FaReact className='m-2 fs-3' />
-            <FaNode className='m-2 fs-3' />
+          <div className="d-flex align-items-center">
+            <div className="me-2">Built with</div>
+            <FaReact className="m-2 fs-3" />
+            <FaNode className="m-2 fs-3" />
           </div>
           <div>
             <div>
-            <FaLinkedin className='m-3 fs-2' />
-            <FaGithub className='m-3 fs-2' />
+              <FaLinkedin className="m-3 fs-2" />
+              <FaGithub className="m-3 fs-2" />
             </div>
           </div>
         </div>
