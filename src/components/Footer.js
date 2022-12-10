c6df7ccd3910d3f00ed69edd9cdf6194488c70db
joshua-mandel <89474480+joshua-mandel@users.noreply.github.com>
2022-12-10 02:08:12 -0600

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,32 @@
 import { FaLinkedin, FaGithub, FaReact, FaNode } from 'react-icons/fa';
+import { SiMongodb, SiExpress } from 'react-icons/si';
 
 function Footer() {
+  const linkedInLink = 'https://www.linkedin.com/in/joshua-mandel-ab382919a/';
+  const gitHubLink = 'https://github.com/joshua-mandel';
+
   return (
     <footer className="bg-dark text-white p-2">
       <div className="container">
         <div className="d-flex align-items-center justify-content-between">
-          <div>&copy; Joshua Mandel 2022</div>
-          <div className="d-flex align-items-center">
-            <div className="me-2">Built with</div>
-            <FaReact className="m-2 fs-3" />
-            <FaNode className="m-2 fs-3" />
+          <div className='my-2'>
+            <div className="my-1  my-2">&copy; Joshua Mandel 2022</div>
+            <div className="d-flex align-items-center my-2">
+              <div className="me-2">Built with</div>
+              <SiMongodb className="m-1 fs-4" />
+              <SiExpress className="m-1 fs-4" />
+              <FaReact className="m-1 fs-4" />
+              <FaNode className="m-1 fs-3" />
+            </div>
           </div>
           <div>
             <div>
-              <FaLinkedin className="m-3 fs-2" />
-              <FaGithub className="m-3 fs-2" />
+              <a className="text-decoration-none text-white" href={linkedInLink} rel="noreferrer" target="_blank">
+                <FaLinkedin className="m-3 fs-1" />
+              </a>
+              <a href={gitHubLink} className="text-white text-decoration-none" rel="noreferrer" target="_blank">
+                <FaGithub className="m-3 fs-1" />
+              </a>
             </div>
           </div>
         </div>
