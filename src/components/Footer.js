c9cf23365a52ff4b03061acfe36d38cca483136d
joshua-mandel <89474480+joshua-mandel@users.noreply.github.com>
2022-12-10 12:54:33 -0600

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,10 @@ import { SiMongodb, SiExpress } from 'react-icons/si';
 function Footer() {
   const linkedInLink = 'https://www.linkedin.com/in/joshua-mandel-ab382919a/';
   const gitHubLink = 'https://github.com/joshua-mandel';
+  const mongoDbLink = 'https://www.mongodb.com/home';
+  const expressJsLink = 'https://expressjs.com/';
+  const reactJsLink = 'https://reactjs.org/';
+  const nodeJsLink = 'https://nodejs.org/en/';
 
   return (
     <footer className="bg-dark text-white p-2">
@@ -13,10 +17,10 @@ function Footer() {
             <div className="my-1  my-2">&copy; Joshua Mandel 2022</div>
             <div className="d-flex align-items-center my-2">
               <div className="me-2">Built with</div>
-              <SiMongodb className="m-1 fs-4" />
-              <SiExpress className="m-1 fs-4" />
-              <FaReact className="m-1 fs-4" />
-              <FaNode className="m-1 fs-3" />
+              <a className="text-decoration-none text-white" href={mongoDbLink} rel="noreferrer" target="_blank"><SiMongodb className="m-1 fs-4" /></a>
+              <a className="text-decoration-none text-white" href={expressJsLink} rel="noreferrer" target="_blank"><SiExpress className="m-1 fs-4" /></a>
+              <a className="text-decoration-none text-white" href={reactJsLink} rel="noreferrer" target="_blank"><FaReact className="m-1 fs-4" /></a>
+              <a className="text-decoration-none text-white" href={nodeJsLink} rel="noreferrer" target="_blank"><FaNode className="m-1 fs-3" /></a>
             </div>
           </div>
           <div>
