e395798fa0513f82b3c6bc42f51d8bfcc7ec68f4
joshua-mandel <89474480+joshua-mandel@users.noreply.github.com>
2022-12-09 22:56:11 -0600

    update

--- src/components/BugEditor.js
+++ src/components/BugEditor.js
@@ -1,4 +1,4 @@
-import { Navigate, useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import InputField from './InputField';
 import DropDown from './DropDown';
