951bc1518cc31ccee556831e8663c43ad6b9458e
joshua-mandel <89474480+joshua-mandel@users.noreply.github.com>
2022-12-14 14:07:59 -0600

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
+if(process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
